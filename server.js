a9ab440200368a76b5ff600b6a5c53ec090e3523
mariappan-subramanian <mariappan.subramanian@freshworks.com>
2021-11-11 22:04:05 +0530

    html added

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 // server.js
 
-// load http module
-const http = require('http')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// load third party Express module
-const express = require('express')
-const app = express()
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// define the routes
-app.get('/', (req, res) => {
-  res.json({title: 'home'})
-})
+app.get('/about', function(req, res) {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
 
-app.get('/about', (req, res) => {
-  res.send({title: 'about'})
-})
-
-// create the server
-const server = http.createServer(app);
-
-// server listen for any incoming requests
-server.listen(3000);
-
-console.log('My node.js web server is alive and running at port 3000')
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
